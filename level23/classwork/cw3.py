f7758e1fb8434f825753cd07f207c9a0ea778bbc
Gvardzelashvili010 <giorgigvardzelashvili2@gmail.com>
2025-07-14 16:03:38 +0300

    1

---
+++ level23/classwork/cw3.py
@@ -0,0 +1,8 @@
+# punkcia aris konkretuli amocanis amoxsna
+# def itereba imitom rom sheickmnas punkcia
+# defis mere itereba rame saxeli da mere prchxilebi  da saboloot 2 tertili :
+# punkcia unda gamovidaxot imistvis rom bevri rame davprintot
+ 
+def hello():
+    print("hello world!")
+hello()
