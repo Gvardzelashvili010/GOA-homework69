24d58f83351dad6ded242bc38e98f15ba1832768
Gvardzelashvili010 <giorgigvardzelashvili2@gmail.com>
2025-07-29 18:18:00 +0300

    1

---
+++ level28/homework/hw.py
@@ -0,0 +1,12 @@
+# 1
+# word = input("შეიყვანე სიტყვა: ")
+# print(word.upper())
+# 2
+# name = input("შეიყვანე შენი სახელი: ")
+# length = len(name)
+# print("შენი სახელის სიგრძეა:", length, "aso")
+# 3
+# list = []
+# list.append(10)
+# print(list)
+# 4
