0b73f537d7d084ca11b04c0213fe0bf77ff3d33d
Gvardzelashvili010 <giorgigvardzelashvili2@gmail.com>
2025-07-30 13:07:53 +0300

    1

---
+++ level26/classwork/cw.py
@@ -0,0 +1,31 @@
+# def is_positive(number):
+#     if number > 0:
+#         return True
+#     else:
+#         return False
+
+
+# def max_of_two(a, b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+
+
+
+# def max_of_three(a, b, c):
+#     if a >= b and a >= c:
+#         return a
+#     elif b >= c:
+#         return b
+#     else:
+#         return c
+
+
+
+
+# def is_hot(temperature):
+#     if temperature >= 30:
+#         return True
+#     else:
+#         return False
